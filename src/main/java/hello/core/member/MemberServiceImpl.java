958a7beda99e1fed0a01d8f3f9ab26df5a966db7
Jeon Hyejin <hyejda00@naver.com>
2024-12-21 15:30:35 +0900

    회원가입과 회원서비스 테스트
    섹션3. 주문과 할인 도메인 설계

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,9 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    // MemberServiceImpl은 MemberRepository라는 인터페이스(추상화)도 의존하고,
+    // MemoryMemberRepository라는 구현체도 의존하고 있어 좋지 않다.
+
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
